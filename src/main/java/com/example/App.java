a9ecf0bb17893fe359b4af8488990a2f7f61269a
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2026-05-09 03:06:12 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,40 +3,18 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
     @GetMapping("/")
     public String home() {
-        return "Welcome to the DevOps Calculator! Use /add, /sub, /mul, or /div?a=10&b=5";
+        return "Hello Microdegree folkss!!";
     }
 
-    @GetMapping("/add")
-    public String add(@RequestParam int a, @RequestParam int b) {
-        return String.format("%d + %d = %d", a, b, (a + b));
-    }
-
-    @GetMapping("/sub")
-    public String sub(@RequestParam int a, @RequestParam int b) {
-        return String.format("%d - %d = %d", a, b, (a - b));
-    }
-
-    @GetMapping("/mul")
-    public String mul(@RequestParam int a, @RequestParam int b) {
-        return String.format("%d * %d = %d", a, b, (a * b));
-    }
-
-    @GetMapping("/div")
-    public String div(@RequestParam int a, @RequestParam int b) {
-        if (b == 0) return "Error: Cannot divide by zero!";
-        return String.format("%d / %d = %.2f", a, b, (double) a / b);
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
